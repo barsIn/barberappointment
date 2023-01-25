e1c64bcf47094217221923e16f4e514132dea591
Igor <bars_in@mail.ru>
2023-01-25 21:26:07 +0700

    Make first command nearlestappoint

---
+++ commands.py
@@ -0,0 +1,116 @@
+import json
+from datetime import datetime, date, time, timedelta
+from jsonfilepy import jsonread, jsonwright
+jsondict = jsonread()
+
+def datToString(thisdate):
+    if thisdate.day < 10:
+        daystr = f'0{thisdate.day}'
+    else:
+        daystr = thisdate.day
+    if thisdate.month < 10:
+        monthstr = f'0{thisdate.month}'
+    else:
+        monthstr = thisdate.month
+    thisdatestr = f'{daystr}.{monthstr}.{thisdate.year}'
+    return thisdatestr
+
+
+def hourStrtoTime(strhour):
+    thour = time(int(strhour[0:2]), int(strhour[3:4]))
+    return thour
+
+def getUnworkTime(dict):
+    unworkTime = []
+    for hour in dict['unwork']['hours']:
+        unworkTime.append(time(int(hour[0:2]), int(hour[3:4])))
+    unworkTime.sort()
+    return (unworkTime)
+
+def getUnworkDays(dict):
+    unworkDays = []
+    for days in dict['unwork']['days']:
+        day = date(int(days[6:]), int(days[3:5]), int(days[0:2]))
+        unworkDays.append(day)
+    return unworkDays
+
+
+def getUnnworkWeekdays(dict):
+    unworkWeekdays = []
+    for weekday in dict['unwork']['weekdays']:
+        unworkWeekdays.append(weekday)
+    return unworkWeekdays
+
+
+
+def checkWorkDay(day, dict):
+    unnworkWeekdays = getUnnworkWeekdays(dict)
+    unworkDays = getUnworkDays(dict)
+    if day.weekday() in unnworkWeekdays:
+        return False
+    elif day in unworkDays:
+        return False
+    else:
+        return True
+
+
+def chekNextApoint(thisday, thistime,  dict):
+    appoints = dict['appoint']
+    daystr = datToString(thisday)
+    dayappoint = appoints.get(daystr, False)
+    workTime = hourStrtoTime(jsondict['unwork']['workhours'][0])
+    if thistime < workTime:
+        thistime = workTime
+    else:
+        thistime = time(thistime.hour+1, 00)
+    if dayappoint:
+        appointtimesstr = dayappoint.keys()
+        appointtimes = []
+        for item in appointtimesstr:
+            item = hourStrtoTime(item)
+            appointtimes.append(item)
+        appointtimes.sort()
+        for item in appointtimes:
+            if thistime >= hourStrtoTime(jsondict['unwork']['workhours'][1]):
+                return False
+            if thistime < item:
+
+                return thisday, thistime
+            else:
+                thistime = time(thistime.hour+1)
+    else:
+        return thisday, thistime
+
+# 'Надо доделать проверку текущего времени на соответсвие рабочим часам, а так же проверить есть ли запись на текущий час'
+
+
+
+
+def nearestEntry(dict):
+    nowtime = time(datetime.now().hour, datetime.now().minute)
+    nowdate = date.today()
+    unWorkHours = hourStrtoTime(jsondict['unwork']['workhours'][1])
+
+    if nowtime > unWorkHours:
+        nowdate = nowdate + timedelta(days=1)
+        nowtime = time(0, 1)
+    while(checkWorkDay(nowdate, dict) == False):
+        nowdate = nowdate + timedelta(days=1)
+        nowtime = time(0, 1)
+    while(True):
+        nextAppointnt = chekNextApoint(nowdate, nowtime, dict)
+        if nextAppointnt:
+            return nextAppointnt
+        else:
+            nowdate = nowdate + timedelta(days=1)
+            nowtime = time(0, 1)
+
+
+
+
+
+# print(getUnnworkWeekdays(jsondict))
+print(nearestEntry(jsondict))
+# print(getUnworkTime(jsondict))
+
+
